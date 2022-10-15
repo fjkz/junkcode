59f2a738e1345fb4ce8e4e08315a3a94ec58ed04
Kazuho Fujii <kazuho.fujii@gmail.com>
2022-10-16 08:04:59 +0900

    intuitive solution

--- nabeatsu/nabeatsu.py
+++ nabeatsu/nabeatsu.py
@@ -1,11 +1,5 @@
 def is_nabeatsu_number(num: int) -> bool:
-    if num % 3 == 0:
-        return True
-    while num > 0:
-        if num % 10 == 3:
-            return True
-        num //= 10
-    return False
+    return num % 3 == 0 or "3" in str(num)
 
 for i in range(1000000):
     if not is_nabeatsu_number(i):
