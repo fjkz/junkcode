e27c21a06e137016a9fb3c4d8459fe691eac5bc8
Kazuho Fujii <kazuho.fujii@gmail.com>
2023-02-25 21:35:39 +0900

    leetcode: 509. Fibonacci Number leetcode: 1137. N-th Tribonacci Number

---
+++ leetcode/1137_TribonacciNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        T = [None] * max(3, n + 1)
+        T[0] = 0
+        T[1] = 1
+        T[2] = 1
+        for i in range(3, n + 1):
+            T[i] = T[i-3] + T[i-2] + T[i-1]
+        return T[n]
