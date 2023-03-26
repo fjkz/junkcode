2b8f2404157afb3f69923569ada0feb3618f257a
Kazuho Fujii <kazuho.fujii@gmail.com>
2023-03-26 18:42:32 +0900

    Apply suggestion from ChatGPT

--- leetcode/25_ReverseNodesInKGroup.py
+++ leetcode/25_ReverseNodesInKGroup.py
@@ -1,19 +1,20 @@
-def hasNextKNodes(node, k):
+def has_k_nodes(node, k):
     work = node
     for i in range(k):
         if work.next:
             work = work.next
-            continue
-        return False
+        else:
+            return False
     return True
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode(next=head)
         khead = dummy
-        while khead and hasNextKNodes(khead, k):
+        while khead and has_k_nodes(khead, k):
             n1 = khead.next
             n2 = n1.next
+            # Swap nodes to reverse the k-sized group
             for i in range(k-1):
                 t = n2.next
                 n2.next = khead.next
