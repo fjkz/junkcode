4b1c624146ccd548f8be6cc5b7cd1bb5ae194d6b
Kazuho Fujii <kazuho.fujii@gmail.com>
2024-01-21 12:04:57 +0900

    leetcode: 198. House Robber

--- leetcode/198_HouseRobber.py
+++ leetcode/198_HouseRobber.py
@@ -1,9 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [None] * len(nums)
+        N = len(nums)
+        if len(nums) <= 2:
+            return max(nums)
+        dp = [None] * N
         dp[0] = nums[0]
-        if len(nums) >= 2:
-            dp[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, N):
             dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-        return dp[-1]
+        return dp[N-1]
