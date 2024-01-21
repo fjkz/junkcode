13942ac0dc678526e56346ab84765aa05674bc36
Kazuho Fujii <kazuho.fujii@gmail.com>
2024-01-21 22:04:06 +0900

    4Sum II better solution.

--- leetcode/454_4SumII.py
+++ leetcode/454_4SumII.py
@@ -2,12 +2,6 @@ from collections import Counter
 
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        count12 = Counter()
-        for n1, n2 in product(nums1, nums2):
-            n12 = n1 + n2
-            count12[n12] += 1
-        count123 = Counter()
-        for (n12, c12), n3 in product(count12.items(), nums3):
-            n123 = n12 + n3
-            count123[n123] += c12
-        return sum(count123[-n4] for n4 in nums4)
+        count12 = Counter(n1 + n2 for n1, n2 in product(nums1, nums2))
+        count34 = Counter(n3 + n4 for n3, n4 in product(nums3, nums4))
+        return sum(c12 * count34[-n12] for n12, c12 in count12.items())
