f5438626b84ee6cfdb74f4f1126673b325f4a821
Kazuho Fujii <kazuho.fujii@gmail.com>
2024-06-30 16:11:01 +0900

    Schema of SaaS application
    documents generated by tbls still have encoding errors.

---
+++ saas_schema/schema.sql
@@ -0,0 +1,191 @@
+-- 資料には言及がない機能の実現のためのテーブルやフィールドはキリがないので省いています。
+-- レコード作成のタイムスタンプなど業務に関係しないフィールドは省略している。
+-- スキーマで表現するために、なるべく外部キー制約を貼っていますが、設計上制約がないところもあります。
+
+CREATE TABLE tenant (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  PRIMARY KEY (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='テナント。主キー以外のフィールドは不明なので省略する。';
+
+CREATE TABLE user (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント。ユーザーは１つのテナントに所属し、テナントの付け換えもないものとする。',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='この SaaS サービスを使う人間';
+
+CREATE TABLE business_partner (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='取引先。請求元や支払い先の個人もしくは法人';
+
+CREATE TABLE bp_bank_account (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  owner int(11) unsigned NOT NULL COMMENT '取引先',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  bank_code char(4) NOT NULL COMMENT '銀行コード。銀行コードのマスタが要るが省略する。',
+  account_type int(1) NOT NULL COMMENT '口座種別',
+  branch char(5) NOT NULL COMMENT '支店番号',
+  account_number char(8) COMMENT '口座番号',
+  PRIMARY KEY (id),
+  FOREIGN KEY (owner) REFERENCES business_partner (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='取引先の銀行口座';
+
+CREATE TABLE payment_method_bank_transfer (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  name char(255) NOT NULL COMMENT '表示名',
+  bank_code char(4) NOT NULL COMMENT '銀行コード。銀行コードのマスタが要るが省略する。',
+  account_type int(1) NOT NULL COMMENT '口座種別',
+  branch char(5) NOT NULL COMMENT '支店番号',
+  account_number char(8) NOT NULL COMMENT '口座番号',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='銀行振り込みによる支払方法. 支払先ではなく、支払い元の銀行口座を示している。支払い方法のカテゴリごとにテーブルを分ける。外部キー制約はつけられないが、ほとんどのフィールドを使わなくて NULL ばかりにならないことを優先する。';
+
+CREATE TABLE payment_method_direct_debit (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='自動引落による支払い方法。フィールドは明らかでないので省略する。';
+
+CREATE TABLE payment_method_credit_card (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='クレジットカードによる支払い方法。フィールドは明らかでないので省略する。';
+
+CREATE TABLE account_category (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  dr_cr_class enum('debit', 'credit') NOT NULL COMMENT '借方 or 貸方',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='科目。科目は独自に追加できるとして、テナント毎に科目を用意する。';
+
+CREATE TABLE account_subcategory (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  category int(11) unsigned NOT NULL COMMENT '親の科目',
+  PRIMARY KEY (id),
+  FOREIGN KEY (category) REFERENCES account_category (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='補助科目';
+
+CREATE TABLE cost_center (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='会計上の部門';
+
+CREATE TABLE tax_class (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  name varchar(255) NOT NULL COMMENT '表示名',
+  PRIMARY KEY (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='税区分. 税率変更を考慮して、ENUM 型で表現せずに別テーブルにする。税区分はテナントによらない。';
+
+CREATE TABLE classification (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  remarks varchar(2000) COMMENT '請求メモ',
+  PRIMARY KEY (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='仕訳. invoice_process にまとめることも考えたが、invoice_process_detail が必要になったので対称性のために仕訳は切り出した。';
+
+CREATE TABLE classification_detail (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  parent int(11) unsigned NOT NULL COMMENT '明細の親である仕訳',
+  date date NOT NULL COMMENT '計上日',
+  amount int(11) NOT NULL COMMENT '金額. 請求を債権と相殺するなどマイナスはありうるので、signed とする。',
+  category int(11) unsigned NOT NULL COMMENT '勘定科目. 科目に借方貸方を設定しているので、科目からこの明細の借方貸方がわかる。',
+  subcategory int(11) unsigned COMMENT '補助科目',
+  cost_center int(11) unsigned COMMENT '部門',
+  tax_class int(11) unsigned NOT NULL COMMENT '税区分',
+  PRIMARY KEY (id),
+  FOREIGN KEY (parent) REFERENCES classification (id),
+  FOREIGN KEY (category) REFERENCES account_category (id),
+  FOREIGN KEY (subcategory) REFERENCES account_subcategory (id),
+  FOREIGN KEY (cost_center) REFERENCES cost_center (id),
+  FOREIGN KEY (tax_class) REFERENCES tax_class (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='仕訳明細。同一の請求明細であっても貸方借方それぞれのレコードが入る。';
+
+CREATE TABLE classification_detail_pair (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  debit_detail int(11) unsigned COMMENT '借方明細。貸方明細と同じ classification に属する。',
+  credit_detail int(11) unsigned COMMENT '貸方明細。貸方明細と同じ classification に属する。',
+  remarks varchar(2000) COMMENT '摘要',
+  PRIMARY KEY (id),
+  FOREIGN KEY (debit_detail) REFERENCES classification_detail (id),
+  FOREIGN KEY (credit_detail) REFERENCES classification_detail (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='仕訳明細ペア. 計上日が同じ借方と借方をペアにして摘要を加えるのは、画面特有の概念と思われるので別テーブルで表現する。';
+
+CREATE TABLE invoice_process (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  state int(2) NOT NULL COMMENT '状態。未処理、削除済、処理中（細かい状態があるかも）、処理済みは必要。',
+  amount int(11) unsigned NOT NULL COMMENT '金額。支払いがマイナスにはならないので、unsigned とする。',
+  payee int(11) unsigned NOT NULL COMMENT '支払先取引先',
+  payment_method_type enum("bank_transfer", "direct_debit", "credit card") NOT NULL COMMENT '支払い方法種別',
+  payment_method_id int(11) unsigned NOT NULL COMMENT '支払い方法。payment_method_type が示すテーブルのID。外部キー制約はつけられない。',
+  payee_bank_account int(11) unsigned COMMENT '振込先口座. payment_method_type=bank_transfer のときのみ値がある',
+  due date NOT NULL COMMENT '支払期日',
+  pic int(11) unsigned NOT NULL COMMENT '担当者',
+  classification int(11) unsigned NOT NULL COMMENT '仕訳',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id),
+  FOREIGN KEY (payee) REFERENCES business_partner (id),
+  FOREIGN KEY (payee_bank_account) REFERENCES bp_bank_account (id),
+  FOREIGN KEY (pic) REFERENCES user (id),
+  FOREIGN KEY (classification) REFERENCES classification (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='請求書処理。請求書の処理と支払いは一貫して行われるものと想定してまとめている。';
+
+CREATE TABLE invoice_file (
+  id int(11) unsigned NOT NULL COMMENT 'ID',
+  tenant int(11) unsigned NOT NULL COMMENT 'テナント',
+  filename varchar(2000) NOT NULL COMMENT 'ファイル名',
+  object_url varchar(2000) NOT NULL COMMENT '請求書のファイルのURL',
+  process_id int(11) unsigned COMMENT 'この請求書が処理されるプロセスのID. 請求書をアップロードしてから、選択して処理すると推測して nullable にする。',
+  PRIMARY KEY (id),
+  FOREIGN KEY (tenant) REFERENCES tenant (id),
+  FOREIGN KEY (process_id) REFERENCES invoice_process (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+COMMENT='請求書ファイル。請求書の中のデータは、構造が取引先によって異なると思われるのでテーブルには入れない。';
