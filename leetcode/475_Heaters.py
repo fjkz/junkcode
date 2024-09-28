1d288124f8c1d9bb114c426470a974be87f97b06
Kazuho Fujii <kazuho.fujii@gmail.com>
2024-09-28 16:42:27 +0900

    Shorten with dummy value technique

--- leetcode/475_Heaters.py
+++ leetcode/475_Heaters.py
@@ -2,15 +2,11 @@ from bisect import bisect
 
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        heaters.sort()
+        INF = 10**11
+        heaters = [- INF] + sorted(heaters) + [INF]
         furthest_house = 0
         for house in houses:
             near = bisect(heaters, house)
-            if near == 0:
-                nearest_heater = heaters[0] - house
-            elif near == len(heaters):
-                nearest_heater = house - heaters[-1]
-            else:
-                nearest_heater = min(house - heaters[near - 1], heaters[near] - house)
+            nearest_heater = min(house - heaters[near - 1], heaters[near] - house)
             furthest_house = max(furthest_house, nearest_heater)
         return furthest_house
