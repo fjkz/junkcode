1f70aa48c287e3d0f6661a603cfc343bfaa7007a
Kazuho Fujii <kazuho.fujii@gmail.com>
2025-09-07 15:50:34 +0900

    1304 one liner

--- leetcode/1304_FindNUniqueIntegersSumUpToZero.py
+++ leetcode/1304_FindNUniqueIntegersSumUpToZero.py
@@ -1,7 +1,3 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        pairs = [ [i, -i] for i in range(1, n // 2 + 1) ]
-        answer = sum(pairs, [])
-        if n % 2 == 1:
-            return [0] + answer
-        return answer
+        return sum(([i, -i] for i in range(1, n // 2 + 1)), [0] if n % 2 else [])
